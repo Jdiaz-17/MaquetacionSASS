400436315662a87ad62e096399ad0c3430bee65f
Juan Diaz <ja.diazg23@uniandes.edu.co>
2022-01-10 12:34:30 -0500

    se cambian de lugar los asset y se agrega el main.js

---
+++ main.js
@@ -0,0 +1,71 @@
+// Cuando no es una funcion se comenta en una sola linea
+let courses = [
+  {
+    name: 'Visualización y Storytelling 1.0',
+    startdate: '24 de enero de 2022',
+    banner: 'assets/asset1.jpg',
+    modality: 'Depende de la Modalidad de la materia que selecciones'
+  },
+  {
+    name: 'Programas Básicos',
+    startdate: '24 de enero de 2022',
+    banner: 'assets/asset1.jpg',
+    modality: 'Modalidad Virtual'
+  },
+  {
+    name: 'Visualización y Storytelling 2.0',
+    startdate: '24 de enero de 2022',
+    banner: 'assets/asset2.jpg',
+    modality: 'Universidad de los Andes - Sede Centro'
+  },
+  {
+    name: 'Alternativas innovadoras de solución  a los retos del ordenamiento',
+    startdate: '24 de enero de 2022',
+    banner: 'assets/asset3.jpg',
+    modality: 'Modalidad Virtual'
+  },
+  {
+    name: 'NARRATIVAS DIGITALES',
+    startdate: '24 de enero de 2022',
+    banner: 'assets/asset4.jpg',
+    modality: 'Depende de la Modalidad de la materia que selecciones'
+  },
+  {
+    name: 'Programa Infantil y Juvenil de Formación Musical',
+    startdate: '24 de enero de 2022',
+    banner: 'assets/asset5.jpg',
+    modality: 'Universidad de los Andes - Sede Centro'
+  },
+  {
+    name: 'Herramientas para formular proyectos inmobiliarios',
+    startdate: '24 de enero de 2022',
+    banner: 'assets/asset6.jpg',
+    modality: 'Modalidad Virtual'
+  },
+]
+
+/** 
+ * Esta función recibe un parametro .....
+ */
+const NombreFuncion = () => {
+  // Manera correcta de declarar una función :)
+  // Se invoca de igual manera NombreFuncion()
+  // No utilizar ; al final de cada sentencia 
+}
+
+
+/// Ejercicios:
+// ---------------------------------------
+/* 
+1. Imprimir en consola, todos los cursos en los que la modalidad es virtual
+2. Imprimir en consola, todos los cursos que la modalidad es Universidad de los andes
+3. Imprimir en consola, todos los cursos en los que depende de la materia. 
+4. Obtener el valor del input "Buscar", y cuando le de clic a buscar, me debe filtrar por el nombre del curso segun el valor que le de.
+5. Se debe crear un formulario, en el que obtenga los campos y cree una card nueva con esos datos :) dejar una imagen por defecto.
+6. Cuando esté en mobile, (window.screen.width) se deben renderizar en forma de tarjeta todos los item del array de objetos courses.
+Renderizar html => https://pretagteam.com/question/using-javascript-function-to-render-html
+
+*/
+// ---------------------------------------
+
+
